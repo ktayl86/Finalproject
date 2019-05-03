a09c6e973c43432bbb4c701f2a40f807f88d6cdb
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-05-03 04:19:51 -0500

    Add files via upload

--- final.js
+++ final.js
@@ -1,6 +1,6 @@
 function init () {
-  // let demoMap = L.Wrld.map('Final', 'e65117ae7fb552545c72547b0314b47d')
-  let demoMap = L.Wrld.map('Final', 'e65117ae7fb552545c72547b0314b47d', {
+  // let demoMap = L.Wrld.map('finalmap', 'e65117ae7fb552545c72547b0314b47d')
+  let demoMap = L.Wrld.map('finalmap', 'e65117ae7fb552545c72547b0314b47d', {
     center: [29.95, -90.07],
     zoom: 15
   })

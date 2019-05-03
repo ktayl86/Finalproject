52968d2b8e29578df7470a4d02ae130d573d2828
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-05-03 06:00:46 -0500

    Add files via upload

--- final.js
+++ final.js
@@ -1,4 +1,4 @@
-let stateMap = L.map('finalmap').setView([29.9511, 90.0715], 3)
+let stateMap = L.map('finalmap').setView([29.9511, -90.0715], 2)
 let basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 L.tileLayer(basemapUrl).addTo(stateMap)
 let streetsBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(stateMap)

56fdf23e76b5d1f0f276828cd97d537009e0946c
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-05-03 03:17:26 -0500

    Update final.js

--- final.js
+++ final.js
@@ -1,6 +1,6 @@
 function init () {
-  // let demoMap = L.Wrld.map('map4', 'e65117ae7fb552545c72547b0314b47d')
-  let demoMap = L.Wrld.map('map4', 'e65117ae7fb552545c72547b0314b47d', {
+  // let demoMap = L.Wrld.map('FinalMap', 'e65117ae7fb552545c72547b0314b47d')
+  let demoMap = L.Wrld.map('FinalMap', 'e65117ae7fb552545c72547b0314b47d', {
     center: [29.95, -90.07],
     zoom: 15
   })
